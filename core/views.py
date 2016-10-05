203c2dac9572937e7bd86bec5bfde451ffd51b5b
ruanbass <ruan.ml@hotmail.com>
2016-10-05 10:22:40 -0300

    Commit Template

---
+++ core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def index(request):
+    context_dict = {'nome_do_prato': "Pizza de Pepperoni"}
+    return render(request, 'index.html', context=context_dict)
