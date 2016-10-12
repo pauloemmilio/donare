71f4ad1ebb2832e0eb34beb264a995654707cdea
ruanbass <ruan.ml@hotmail.com>
2016-10-12 12:13:28 -0300

    Models Ong

---
+++ ong/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Ong(models.Model):
+    name = models.CharField(max_length=200)
+    categoria = models.CharField(max_length=100)
+    cnpj = models.IntegerField()
+    telefone = models.CharField(max_length=100)
+    email = models.EmailField()
+    senha = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=200)
+    agencia = models.IntegerField()
+    conta = models.IntegerField()
+    nomeTitular = models.CharField(max_length=200)
+    fotos = models.ImageField(
+        null = True,
+        blank = True,
+        upload_to = 'static/img/ong',
+    )
+    videoUrl = models.URLField()
+    descricao = models.CharField(max_length=500)
+    def __str__(self):
+        return self.nome
