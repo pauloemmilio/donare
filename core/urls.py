4dcd9dad8a6713cbf43710c431b8951210265c9f
pauloemmilio <paulo.emilio@dce.ufpb.br>
2016-10-12 12:12:20 -0300

    Atualização do css da página das ONGS e alteração das URLs e Views

--- core/urls.py
+++ core/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from core import views
 urlpatterns = [
  url(r'^$', views.index, name='index'),
+ url(r'^ong/$', views.ong, name='ong'),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'nome_do_prato': "Pizza de Pepperoni"}
     return render(request, 'index.html', context=context_dict)
+
+def ong(request):
+	return render(request, 'projects.html')
