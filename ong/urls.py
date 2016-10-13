6390e72bd0837f3ac5823b80a71a8f0183e9a1ce
ruanbass <ruan.ml@hotmail.com>
2016-10-13 14:03:24 -0300

    Crud das Views

--- ong/urls.py
+++ ong/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from ong import views
 urlpatterns = [
 
- url(r'^$', views.ongs_list, name='ongs_list'),
- url(r'^new$', views.ResistrarOng.as_view(), name='criar_ong'),
+ url(r'^$', views.index, name='index'),
+ url(r'^new', views.criar_ong, name='criar'),
  url(r'^edit/(?P<pk>\d+)$', views.alterar_ong, name='alterar_ong'),
  url(r'^delete/(?P<pk>\d+)$', views.deletar_ong, name='deletar_ong'),
 ]
