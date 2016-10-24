cd862469b067221407dea31c0df40bf4153cb671
pauloemmilio <paulo.emilio@dce.ufpb.br>
2016-10-24 20:26:25 -0300

    Finalização da visualização de doador no back-end

--- doador/views.py
+++ doador/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-from django.forms import ModelForm
 from doador.forms import DoadorForm
+from doador.models import Doador
+
+from django.forms import ModelForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.views.generic import TemplateView,ListView, CreateView
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+
+def profile(request, doador_id):
+	doador = Doador.objects.get(pk=doador_id)
+	context_dict = {'doador': doador}
+	return render(request, 'profile.html', context=context_dict)
 
 def cadastrar_doador(request):
 
